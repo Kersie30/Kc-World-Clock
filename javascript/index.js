8b0672c26de84a212b1a73d9073a40133e3ae8e6
Kersie30 <keresiakaruma@gmail.com>
2024-11-12 23:24:51 +0200

    updated JS

--- javascript/index.js
+++ javascript/index.js
@@ -23,3 +23,23 @@ function updateParisTime() {
   parisTimeElement.innerHTML = paris.format("h:mm:ss[<small>]A[</small>]");
 }
 setInterval(updateParisTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+<div class="city">
+<div>
+ <h2>${cityTimeZone}</h2>
+<div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+</div>
+<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+</div>
+`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
